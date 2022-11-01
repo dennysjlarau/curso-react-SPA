ba4e9ba536707590ae702f152c32dd626d7b2071
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-11-01 09:20:57 -0500

    visualización de imagenes

--- src/heroes/components/HeroeList.jsx
+++ src/heroes/components/HeroeList.jsx
@@ -1,4 +1,5 @@
 import { getHeroesByPublisher } from "../helpers"
+import { HeroCard } from "./";
 
 
 
@@ -6,14 +7,15 @@ export const HeroeList = ({publisher}) => {
     const heroes = getHeroesByPublisher(publisher);
 
   return (
-    <ul >
+    <div className="row rows-cols-1 row-cols-md-3 g-3" >
       {
         heroes.map( hero => (
-          <li key={hero.id}>
-            {hero.superhero}
-          </li>
+          <HeroCard 
+            key={ hero.id }
+            {...hero}
+          />
         ))
       } 
-    </ul>
+    </div>
   )
 }
